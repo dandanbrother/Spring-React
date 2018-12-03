ba13d523c7065556fd0937d2fb703a1bdf683cb0
dandanbrother <nidingfan1@gmail.com>
2018-12-03 15:41:15 -0800

    crud

--- PPMTool/src/main/java/io/dylan/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/io/dylan/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.dylan.ppmtool.services;
 
 import io.dylan.ppmtool.domain.Project;
+import io.dylan.ppmtool.exception.ProjectIdException;
 import io.dylan.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,31 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier() + "' already exists.");
+        }
+    }
+
+    public Project findByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' doesn't exist.");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete project with ID '" + projectId.toUpperCase() + "' not exists.");
+        }
+        projectRepository.delete(project);
     }
 }
--- PPMTool/src/main/java/io/dylan/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/io/dylan/ppmtool/web/ProjectController.java
@@ -1,15 +1,13 @@
 package io.dylan.ppmtool.web;
 
 import io.dylan.ppmtool.domain.Project;
+import io.dylan.ppmtool.services.MapValidationErrorService;
 import io.dylan.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,13 +18,32 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+        if (errorMap != null) return errorMap;
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+        Project project = projectService.findByIdentifier(projectId);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<>("Project deleted.", HttpStatus.OK);
     }
 }
